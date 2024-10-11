1f35ba0df66968fa43d9b003ecc0f93524296607
Ahmed Abdelraheem <ahmedkinga60@gmail.com>
2024-10-11 21:58:05 +0300

    v4

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --timeout 120 -k eventlet -w 1 app:app
+web: gunicorn --worker-class eventlet -w 1 app:app
--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    socketio.run(app, host='127.0.0.1', port=8080)
+    socketio.run(app, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
